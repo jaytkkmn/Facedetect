4a445a55932436ac61073d7e7135bc9e55541f33
jaytkkmn <jaytkkmn@msn.com>
2019-09-26 17:59:40 +0800

    Init version * Face detection by webcam

---
+++ videocapture.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+
+
+# Load the model
+net = cv.dnn.readNet('face-detection-adas-0001.xml', 'face-detection-adas-0001.bin')
+
+# Specify target device
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+cap = cv.VideoCapture(0)
+
+# Draw detected faces on the frame
+
+while(True):
+  ret, frame = cap.read()
+  # Prepare input blob and perform an inference
+  blob = cv.dnn.blobFromImage(frame, size=(672, 384), ddepth=cv.CV_8U)
+  net.setInput(blob)
+  out = net.forward()
+
+  for detection in out.reshape(-1, 7):
+      confidence = float(detection[2])
+      xmin = int(detection[3] * frame.shape[1])
+
+      ymin = int(detection[4] * frame.shape[0])
+      xmax = int(detection[5] * frame.shape[1])
+      ymax = int(detection[6] * frame.shape[0])
+      if confidence > 0.5:
+          cv.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
+
+  cv.imshow("Video", frame)
+
+  if cv.waitKey(1) & 0xFF == ord('q'):
+    break
+
+cap.release()
+cv.destroyAllWindows()
