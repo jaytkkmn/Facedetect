bd5b4d2c1e0b69d810c2cb7a47c9a9ae01e1fa01
jaytkkmn <jaytkkmn@msn.com>
2019-09-27 15:05:23 +0800

    Init version * Face detection and if detect face will trigger to save image

--- videocapture.py
+++ videocapture.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-
+from time import gmtime, strftime
 
 # Load the model
 net = cv.dnn.readNet('face-detection-adas-0001.xml', 'face-detection-adas-0001.bin')
@@ -9,8 +9,8 @@ net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
 cap = cv.VideoCapture(0)
 
+counter = 0
 # Draw detected faces on the frame
-
 while(True):
   ret, frame = cap.read()
   # Prepare input blob and perform an inference
@@ -18,15 +18,22 @@ while(True):
   net.setInput(blob)
   out = net.forward()
 
+
   for detection in out.reshape(-1, 7):
       confidence = float(detection[2])
       xmin = int(detection[3] * frame.shape[1])
-
       ymin = int(detection[4] * frame.shape[0])
       xmax = int(detection[5] * frame.shape[1])
       ymax = int(detection[6] * frame.shape[0])
+
       if confidence > 0.5:
           cv.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
+          counter = counter + 1
+          if counter > 100:
+              save_name = "img_" + strftime("%Y-%m-%d_%H%M%S") + ".jpg"
+              cv.imwrite(save_name, frame)
+              counter = 0
+
 
   cv.imshow("Video", frame)
 
